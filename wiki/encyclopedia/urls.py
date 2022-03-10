f4d124bfe8a21512242f0111c8a5635e198c0c95
Martina Quintas <martinaquintas.w@gmail.com>
2022-03-10 15:26:42 -0300

    search step 1

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wiki'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.title, name="by_title")
+    path("search", views.search, name="search"),
+    path("wiki/<str:title>", views.title, name="by_title"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 import markdown2
 
 
@@ -14,12 +15,6 @@ def index(request):
 def title(request, title):
     entry = util.get_entry(title)
     if entry == None:
-        # render search result
-        # search substrings to lower de los dos lados.
-        # entries_names = [CSS, Django]
-        # find in entries_name [title]
-        # each{ entries_name[i].lower == title.lower} => entries_name[i]
-        # render list entries_names_search_result un subset de entries_names como te lo da util.py
         return HttpResponse(f"404 - {title} - was not found")
     else:
         entry = markdown2.markdown(util.get_entry(title))
@@ -27,3 +22,21 @@ def title(request, title):
             "title": title,
             "entry": entry,
         })
+
+def search(request):
+    search = request.GET["q"]
+    entry = util.get_entry(search)
+
+    if entry == None:
+
+            # render search result
+            # search substrings to lower de los dos lados.
+            # entries_names = [CSS, Django]
+            # find in entries_name [title]
+            # each{ entries_name[i].lower == title.lower} => entries_name[i]
+            # render list entries_names_search_result un subset de entries_names como te lo da util.py
+        # search substrings
+        return HttpResponse(f"Hello World {request.GET['q']}")
+    else:
+        return redirect(f'wiki/{search}')
+    print (search)
